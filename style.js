d78e643b70f73bad5d6e7aef2204e76c34e46aac
nastiknas <miledynastia@gmail.com>
2025-03-27 00:23:22 +0300

    Добавила 11 домашнее задание

---
+++ style.js
@@ -0,0 +1,35 @@
+// Позиция 1
+//    CSS       button.sc-d6c9e5b8-3.sc-d6c9e5b8-4.eTbKet.hGNtcn
+//    XPATH     //button[@class="sc-d6c9e5b8-3 sc-d6c9e5b8-4 eTbKet hGNtcn"]
+
+// Позиция 2
+//    CSS       nav.sc-f116bf72-1.xECcw
+//    XPATH     //nav[@class="sc-f116bf72-1 xECcw"]
+
+// Позиция 3
+//    CSS     div.sc-93223220-0.sc-37dbbdae-1.gPmTJa.cCwylM div.sc-530fb3d6-1.QTNSr span.sc-6fba5bd4-1.heskfp
+//                                      или
+//            [data-testid="first-grid"] [data-testid="dundee-card"]:nth-of-type(1) [data-testid="card-metadata-lastupdated"]                         
+//  
+//   XPATH   //div[@class="sc-93223220-0 sc-37dbbdae-1 gPmTJa cCwylM"]//div[@class="sc-530fb3d6-1 QTNSr"]//span[@class="sc-6fba5bd4-1 heskfp"]
+//                                      или
+//              (//div[2]//span[@class="sc-6fba5bd4-1 heskfp"])[1]
+
+// Позиция 4
+//    CSS       div.sc-9f7cc048-5.hwyuZn
+//    XPATH    //div[@class="sc-9f7cc048-5 hwyuZn"]
+
+// Позиция 6
+//    CSS     [data-testid="second-grid"] [data-testid="manchester-card"]:nth-of-type(1) [data-testid="card-metadata-lastupdated"]                       
+//  
+//   XPATH   (//div[@class="sc-93223220-0 sc-37dbbdae-3 jPpGMu bnXQIy"]//span[1])[1]
+//                                      или
+//              (//div[@class="sc-c6f6255e-0 eGcloy"]//span[1])[6]
+ 
+// Позиция 7
+//    CSS       [data-testid="second-grid"] [data-testid="manchester-card"]:nth-of-type(4)
+//    XPATH     //div[@data-testid="second-grid"]/div[4]
+
+// Позиция Sign in
+//    CSS        button.sc-b5722a8e-2.sc-b5722a8e-5.fmMewN.hzScvu
+//    XPATH      //button[@class="sc-b5722a8e-2 sc-b5722a8e-5 fmMewN hzScvu"]
