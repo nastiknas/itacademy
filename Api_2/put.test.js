53aeebebf30e5f00e2c8c688f588abd0c75dddcf
nastiknas <miledynastia@gmail.com>
2025-03-30 20:09:38 +0300

    Update put.test.js

--- Api_2/put.test.js
+++ Api_2/put.test.js
@@ -36,19 +36,4 @@ describe('Api test', () => {
      await expect(response.data).toBeTruthy();
 })
 
-  test('PUT should be able to list of Activities (failed test status code)', async () => {
-   const response = await axios.put("https://fakerestapi.azurewebsites.net/api/v1/Activities/1",
-    {
-    
-        "id": 0,
-        "title": "string",
-        "dueDate": "2025-03-20T12:01:03.457Z",
-        "completed": true
-  
-     })
-     console.log(response)
-     await expect(response.status).toEqual(400);
-     await expect(response.data).toBeTruthy();
-})
-
 })
